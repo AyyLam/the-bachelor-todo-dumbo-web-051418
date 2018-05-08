c250bc4b1a111634bd2622e4cfa401c4fdc01347
Anthony Lam <al3101@nyu.edu>
2018-05-08 18:41:44 +0000

    automatically backed up by learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -40,7 +40,15 @@ def count_contestants_by_hometown(data, hometown)
 end
 
 def get_occupation(data, hometown)
-  # code here
+  data.each do |season_number, season_data| 
+    season_data.each do |person|
+      person.each do |condition, value| 
+          if condition == "hometown" && value == hometown
+            counter += 1
+          end
+        end
+      end
+    end
 end
 
 def get_average_age_for_season(data, season)
