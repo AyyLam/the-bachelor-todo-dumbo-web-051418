9c82a69756a877170a92081a2131a69b1d150499
Anthony Lam <al3101@nyu.edu>
2018-05-08 18:48:40 +0000

    automatically backed up by learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -57,7 +57,7 @@ def get_average_age_for_season(data, season)
   data.each do |season_number, season_data| 
     if season_number == season 
       season_data.each do |person|
-        total_age += person["age"].to_f.ceil
+        total_age += (person)["age"].to_f).ceil
         total_people += 1
       end
     end
